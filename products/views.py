cbb6137bf5df3b8ff16be43d09529d61fcc5b085
besesito <bartekszmude@gmail.com>
2021-01-08 01:09:47 +0100

    Added product model

--- products/views.py
+++ products/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Product
+from django.utils import timezone
+
 
 
 def home(request):
     products = Product.objects
     return render(request, 'products/home.html', { 'products':products })
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.FILES['icon'] and request.POST['body'] and request.FILES['picture'] and request.POST['url']:
+            product = Product()
+            product.title = request.POST['title']
+            product.icon = request.FILES['icon']
+            product.body = request.POST['body']
+            product.picture = request.FILES['picture']
+            product.url = request.POST['url']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+        else:
+            return render(request, 'products/create.html', {'info':'Please fill all fields'})
+    else:
+        return render(request, 'products/create.html')
